a930bee0c0fb3a207c5f751f301e2ec638a61f33
Zeki <zckural@gmail.com>
2021-10-31 02:03:30 -0400

    demo user added

--- frontend/components/session/login.jsx
+++ frontend/components/session/login.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React from "react";
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.demoUser = this.demoUser.bind(this);
+  }
+
+  demoUser(e) {
+    e.preventDefault();
+    this.setState(
+      {
+        email: "demo@alljourneys.com",
+        password: "demouser123",
+      },
+      () => this.handleSubmit(e)
+    );
   }
 
   handleInput(type) {
@@ -19,7 +31,7 @@ class Login extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    this.props.login(this.state)
+    this.props.login(this.state);
   }
 
   render() {
@@ -27,23 +39,26 @@ class Login extends React.Component {
       <div className="session-form">
         <h2>Log In!</h2>
         <form>
-          <label>Email:
-          <input
-            type="text"
-            value={this.state.email}
-            onChange={this.handleInput('email')}
-          />
+          <label>
+            Email:
+            <input
+              type="text"
+              value={this.state.email}
+              onChange={this.handleInput("email")}
+            />
           </label>
 
-          <label>Password:
-          <input
-            type="password"
-            value={this.state.password}
-            onChange={this.handleInput('password')}
-          />
+          <label>
+            Password:
+            <input
+              type="password"
+              value={this.state.password}
+              onChange={this.handleInput("password")}
+            />
             <button onClick={this.handleSubmit}>Log In!</button>
           </label>
         </form>
+        <button onClick={this.demoUser}>Demo User</button>
       </div>
     );
   }
